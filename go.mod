ac559c487ffd8a36a67690647b66023063eebd57
Marco <marco_antonios1@outlook.com>
2024-05-06 18:52:20 +0300

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module userrelation
+
+go 1.22.2
+
+require go.mongodb.org/mongo-driver v1.15.0
---
+++ model/model.go
@@ -0,0 +1,29 @@
+package model
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type User struct {
+	ID             primitive.ObjectID `json:"_id" bson:"_id"`
+	UserName       *string            `json:"username" bson:"username" validate:"required,min=4,max=30"`
+	Name           *string            `json:"name" bson:"name"`
+	Image          *string            `json:"image" bson:"image"`
+	Password       *string            `json:"password" bson:"password" validate:"required,min=8"`
+	Email          *string            `json:"email" bson:"email" validate:"email,required"`
+	Phone          *string            `json:"phone" bson:"phone" validate:"required"`
+	Gender         *string            `json:"gender" bson:"gender"`
+	Biography      *string            `json:"bio" bson:"bio"`
+	Followers      uint32             `json:"followers" bson:"followers"`
+	Following      uint32             `json:"following" bson:"following"`
+	Status         bool               `json:"status" bson:"status"`
+	Private        bool               `json:"private" bson:"private"`
+	Token          *string            `json:"access_token" bson:"access_token"`
+	Refresh_Token  *string            `json:"refresh_token" bson:"refresh_token"`
+	Email_Verified bool               `json:"email_verified" bson:"email_verified"`
+	Birthday       time.Time          `json:"birthday" bson:"birthday"`
+	Created_At     time.Time          `json:"created_at" bson:"created_at"`
+	Updated_At     time.Time          `json:"updated_at" bson:"updated_at"`
+}
