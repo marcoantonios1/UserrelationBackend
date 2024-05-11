ad2ec567f47438fcdbe7a0a7ccfaf19ba8032c27
Marco <marco_antonios1@outlook.com>
2024-05-11 20:42:37 +0300

    neo4j and check if follow

--- go.mod
+++ go.mod
@@ -35,7 +35,10 @@ require (
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/neo4j/neo4j-go-driver v1.8.3
+	github.com/neo4j/neo4j-go-driver/v5 v5.20.0
 	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
+	github.com/rs/cors v1.11.0
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
 	golang.org/x/arch v0.3.0 // indirect
--- main.go
+++ main.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/rs/cors"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+
+	c := cors.New(cors.Options{
+		AllowedOrigins: []string{"http://localhost:4200"},
+		AllowedMethods: []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowedHeaders: []string{"Origin", "Content-Type", "Authorization"},
+	})
+	handler := c.Handler(router)
+
+	//route.Routes(router)
+
+	log.Fatal(http.ListenAndServe(":"+port, handler))
+}
