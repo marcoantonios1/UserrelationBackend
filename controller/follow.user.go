0b73fa7c5c19138fb44d578f79a386e5184a21f7
Marco <marco_antonios1@outlook.com>
2024-05-12 18:06:15 +0300

    chore: Remove unused Kafka helper import and function calls

--- controller/follow.user.go
+++ controller/follow.user.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"userrelation/database"
+	"userrelation/helper"
 
 	"github.com/gin-gonic/gin"
 	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
@@ -251,50 +252,7 @@ func Follow() gin.HandlerFunc {
 		}
 
 		c.JSON(http.StatusOK, "Followed")
-	}
-}
-
-func AcceptRequest() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		// Get user ID from context
-		userID, exists := c.Get("id")
-		if !exists {
-			c.JSON(http.StatusBadRequest, gin.H{"error": "User ID not found in context"})
-			return
-		}
-
-		userIDObj, ok := userID.(primitive.ObjectID)
-		if !ok {
-			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid user ID"})
-			return
-		}
-
-		userToFollowID := c.Query("user_id")
-		userToFollowIDObj, err := primitive.ObjectIDFromHex(userToFollowID)
-		if err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid user ID to follow"})
-			return
-		}
-
-		ctx := context.Background() // Consider using request-scoped context
-
-		// Increment the 'following' count of the user
-		update := bson.M{"$inc": bson.M{"following": 1}}
-		_, err = UsersCollection.UpdateOne(ctx, bson.M{"_id": userToFollowIDObj}, update)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update user"})
-			return
-		}
-
-		// Increment the 'followers' count of the user being followed
-		update = bson.M{"$inc": bson.M{"followers": 1}}
-		_, err = UsersCollection.UpdateOne(ctx, bson.M{"_id": userIDObj}, update)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update user to follow"})
-			return
-		}
-
-		c.JSON(http.StatusOK, "Followed")
+		go helper.KafkaFollow(ctx, userIDObj.Hex(), userToFollowID)
 	}
 }
 
@@ -339,5 +297,126 @@ func UnFollow() gin.HandlerFunc {
 		}
 
 		c.JSON(http.StatusOK, "Unfollowed")
+		go helper.KafkaUnFollow(ctx, userIDObj.Hex(), userToUnFollowID)
+	}
+}
+
+func FollowRequest() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// Get user ID from context
+		userID, exists := c.Get("id")
+		if !exists {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "User ID not found in context"})
+			return
+		}
+
+		userIDObj, ok := userID.(primitive.ObjectID)
+		if !ok {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid user ID"})
+			return
+		}
+
+		userToFollowID := c.Query("user_id")
+		userToFollowIDObj, err := primitive.ObjectIDFromHex(userToFollowID)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid user ID to follow"})
+			return
+		}
+
+		ctx := context.Background() // Consider using request-scoped context
+
+		// Increment the 'followers' count of the user being followed
+		update := bson.M{"$inc": bson.M{"follow_requests": 1}}
+		_, err = UsersCollection.UpdateOne(ctx, bson.M{"_id": userToFollowIDObj}, update)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update user to follow"})
+			return
+		}
+
+		c.JSON(http.StatusOK, "Requested")
+		go helper.KafkaFollowRequest(ctx, userIDObj.Hex(), userToFollowID)
+	}
+}
+
+func AcceptRequest() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// Get user ID from context
+		userID, exists := c.Get("id")
+		if !exists {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "User ID not found in context"})
+			return
+		}
+
+		userIDObj, ok := userID.(primitive.ObjectID)
+		if !ok {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid user ID"})
+			return
+		}
+
+		userToFollowID := c.Query("user_id")
+		userToFollowIDObj, err := primitive.ObjectIDFromHex(userToFollowID)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid user ID to follow"})
+			return
+		}
+
+		ctx := context.Background() // Consider using request-scoped context
+
+		// Increment the 'following' count of the user
+		update := bson.M{"$inc": bson.M{"following": 1}}
+		_, err = UsersCollection.UpdateOne(ctx, bson.M{"_id": userToFollowIDObj}, update)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update user"})
+			return
+		}
+
+		// Increment the 'followers' count of the user being followed
+		update = bson.M{"$inc": bson.M{"followers": 1}}
+		_, err = UsersCollection.UpdateOne(ctx, bson.M{"_id": userIDObj}, update)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update user to follow"})
+			return
+		}
+
+		c.JSON(http.StatusOK, "Followed")
+		go helper.KafkaAcceptFollowRequest(ctx, userIDObj.Hex(), userToFollowID)
+	}
+}
+
+func DeclineRequest() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// Get user ID from context
+		userID, exists := c.Get("id")
+		if !exists {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "User ID not found in context"})
+			return
+		}
+
+		userIDObj, ok := userID.(primitive.ObjectID)
+		if !ok {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid user ID"})
+			return
+		}
+
+		userToFollowID := c.Query("user_id")
+		userToFollowIDObj, err := primitive.ObjectIDFromHex(userToFollowID)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid user ID to follow"})
+			return
+		}
+
+		ctx := context.Background() // Consider using request-scoped context
+
+		// Increment the 'followers' count of the user being followed
+		update := bson.M{"$inc": bson.M{"follow_requests": -1}}
+		_, err = UsersCollection.UpdateOne(ctx, bson.M{"_id": userToFollowIDObj}, update)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update user to follow"})
+			return
+		}
+
+		c.JSON(http.StatusOK, "Followed")
+
+		go helper.KafkaDeclineFollowRequest(ctx, userIDObj.Hex(), userToFollowID)
 	}
 }
