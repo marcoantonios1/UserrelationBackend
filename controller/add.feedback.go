4168d74c804cfea13a84d0b4ccd1ac6d057954b1
Marco <marco_antonios1@outlook.com>
2024-10-24 21:45:21 +0300

    chore: Add LocationCollection to retrieve location data

--- controller/add.feedback.go
+++ controller/add.feedback.go
@@ -14,6 +14,7 @@ import (
 )
 
 var FeedbackCollection *mongo.Collection = database.OrdersData(database.Orders, "Feedbacks")
+var LocationCollection *mongo.Collection = database.RestaurantsData(database.Restaurants, "Location")
 
 func AddFeedback() gin.HandlerFunc {
 	return func(c *gin.Context) {
@@ -52,11 +53,13 @@ func AddFeedback() gin.HandlerFunc {
 			return
 		}
 
-		// Validate rating value
 		if feedback.Rating < 1 || feedback.Rating > 5 {
 			c.JSON(http.StatusBadRequest, gin.H{"error": "Rating should be between 1 and 5"})
 			return
 		}
+
+		// Ensure feedback.Rating is a float32
+		feedbackRating := float32(feedback.Rating)
 
 		// Populate feedback object with IDs and timestamp
 		feedback.ID = primitive.NewObjectID()
@@ -66,7 +69,7 @@ func AddFeedback() gin.HandlerFunc {
 		feedback.Reservation_ID = reservationIDObj
 		feedback.Created_At = time.Now()
 
-		// Insert feedback into the orderCollection
+		// Insert feedback into the FeedbackCollection
 		_, err = FeedbackCollection.InsertOne(c, feedback)
 		if err != nil {
 			log.Print(err)
@@ -74,8 +77,76 @@ func AddFeedback() gin.HandlerFunc {
 			return
 		}
 
+		// Update restaurant's rating and total reviews atomically
+		restaurantPipeline := mongo.Pipeline{
+			{{Key: "$set", Value: bson.D{
+				{Key: "total_reviews", Value: bson.D{
+					{Key: "$add", Value: bson.A{
+						bson.D{{Key: "$ifNull", Value: bson.A{"$total_reviews", 0}}},
+						1,
+					}},
+				}},
+			}}},
+			{{Key: "$set", Value: bson.D{
+				{Key: "rating", Value: bson.D{
+					{Key: "$add", Value: bson.A{
+						bson.D{{Key: "$ifNull", Value: bson.A{bson.D{{Key: "$toDouble", Value: "$rating"}}, 0.0}}},
+						bson.D{{Key: "$divide", Value: bson.A{
+							bson.D{{Key: "$subtract", Value: bson.A{
+								feedbackRating,
+								bson.D{{Key: "$ifNull", Value: bson.A{bson.D{{Key: "$toDouble", Value: "$rating"}}, 0.0}}},
+							}}},
+							bson.D{{Key: "$toDouble", Value: "$total_reviews"}},
+						}}},
+					}},
+				}},
+				{Key: "updated_at", Value: time.Now()},
+			}}},
+		}
+
+		// Update restaurant document
+		_, err = RestaurantCollection.UpdateByID(c, restaurantIDObj, restaurantPipeline)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error updating restaurant rating"})
+			return
+		}
+
+		// Update location's rating and total reviews atomically
+		locationPipeline := mongo.Pipeline{
+			{{Key: "$set", Value: bson.D{
+				{Key: "total_reviews", Value: bson.D{
+					{Key: "$add", Value: bson.A{
+						bson.D{{Key: "$ifNull", Value: bson.A{"$total_reviews", 0}}},
+						1,
+					}},
+				}},
+			}}},
+			{{Key: "$set", Value: bson.D{
+				{Key: "rating", Value: bson.D{
+					{Key: "$add", Value: bson.A{
+						bson.D{{Key: "$ifNull", Value: bson.A{bson.D{{Key: "$toDouble", Value: "$rating"}}, 0.0}}},
+						bson.D{{Key: "$divide", Value: bson.A{
+							bson.D{{Key: "$subtract", Value: bson.A{
+								feedbackRating,
+								bson.D{{Key: "$ifNull", Value: bson.A{bson.D{{Key: "$toDouble", Value: "$rating"}}, 0.0}}},
+							}}},
+							bson.D{{Key: "$toDouble", Value: "$total_reviews"}},
+						}}},
+					}},
+				}},
+				{Key: "updated_at", Value: time.Now()},
+			}}},
+		}
+
+		// Update location document
+		_, err = LocationCollection.UpdateByID(c, locationIDObj, locationPipeline)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error updating location rating"})
+			return
+		}
+
 		// Return success response
-		c.JSON(http.StatusOK, "Feedback submitted successfully")
+		c.JSON(http.StatusOK, gin.H{"message": "Feedback submitted and ratings updated successfully"})
 	}
 }
 
--- model/model.go
+++ model/model.go
@@ -42,10 +42,13 @@ type Neo4jUser struct {
 }
 
 type Restaurant struct {
-	Restaurant_ID   string `json:"_id" bson:"_id"`
-	Restaurant_Name string `json:"restaurant_name" bson:"restaurant_name" validate:"required,min=2,max=30"`
-	Description     string `json:"description" bson:"description" validate:"required,min=2,max=30"`
-	Image           string `json:"image" bson:"image"`
+	Restaurant_ID   string   `json:"_id" bson:"_id"`
+	Restaurant_Name string   `json:"restaurant_name" bson:"restaurant_name" validate:"required,min=2,max=30"`
+	Description     string   `json:"description" bson:"description" validate:"required,min=2,max=30"`
+	Image           string   `json:"image" bson:"image"`
+	Price           *float32 `json:"price,omitempty" bson:"price,omitempty"`
+	Rating          *float32 `json:"rating,omitempty" bson:"rating,omitempty"`
+	Total_Reviews   uint32   `json:"total_reviews,omitempty" bson:"total_reviews,omitempty"`
 }
 
 type Feedback struct {
@@ -57,4 +60,30 @@ type Feedback struct {
 	Feedback       string             `json:"feedback,omitempty" bson:"feedback,omitempty"`
 	Rating         uint32             `json:"rating" bson:"rating"`
 	Created_At     time.Time          `json:"created_at" bson:"created_at"`
+}
+
+type Location struct {
+	ID                       primitive.ObjectID `json:"_id" bson:"_id"`
+	RestaurantID             primitive.ObjectID `bson:"restaurant_id"`
+	Name                     *string            `json:"name" bson:"name" validate:"required"`
+	Locality                 *string            `json:"locality" bson:"locality" validate:"required"`
+	AdministrativeAreaLevel2 *string            `json:"administrative_area_level_2" bson:"administrative_area_level_2" validate:"required"`
+	AdministrativeAreaLevel1 *string            `json:"administrative_area_level_1" bson:"administrative_area_level_1" validate:"required"`
+	Country                  *string            `json:"country" bson:"country" validate:"required"`
+	Address                  *string            `json:"address" bson:"address" validate:"required"`
+	Lat                      *float64           `json:"lat" bson:"lat" validate:"required"`
+	Long                     *float64           `json:"long" bson:"long" validate:"required"`
+	Price                    *float32           `json:"price,omitempty" bson:"price,omitempty"`
+	Rating                   *float32           `json:"rating,omitempty" bson:"rating,omitempty"`
+	Total_Reviews            uint32             `json:"total_reviews,omitempty" bson:"total_reviews,omitempty"`
+	Closed                   bool               `json:"closed" bson:"closed"`
+	Delivery                 bool               `json:"delivery" default:"false" bson:"delivery"`
+	Pickup                   bool               `json:"pickup" default:"false" bson:"pickup"`
+	EatIn                    bool               `json:"eat_in" default:"false" bson:"eat_in"`
+	MinCharge                bool               `json:"min_charge" default:"false" bson:"min_charge"`
+	MinChargePerPerson       *uint8             `json:"min_charge_per_pers,omitempty" bson:"min_charge_per_pers,omitempty"`
+	AlwaysOpen               bool               `json:"always_open" default:"false" bson:"always_open"`
+	MaxDeliveryDistanceKm    float64            `json:"max_delivery_distance_km" bson:"max_delivery_distance_km"` // New field
+	CreatedAt                time.Time          `json:"created_at" bson:"created_at"`
+	UpdatedAt                time.Time          `json:"updated_at" bson:"updated_at"`
 }
